37dfa3997a0187dc801534b3fbfaa6f86b9c0249
kakhramon007 <soatovkahramon3@gmail.com>
2025-02-13 16:46:20 +0500

    Added some commets in the main.py

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from tkinter.ttk import  *
 
 from time import strftime
 
+# Creating variable from TK
 root = Tk()
 root.title("Clock")
 
+# Function to format the time
 def time():
     string = strftime('%H:%M:%S %p')
     label.config(text=string)
     label.after(1000, time)
 
+# Showing time
 label = Label(root, font=("ds_digital", 80), background = "black", foreground = "cyan" )
 label.pack(anchor='center')
 time()
